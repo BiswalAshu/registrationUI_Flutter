491d39efcca2a7f7985b9f2810603f5edc2b0963
BiswalAshu <aaronstone201299@gmail.com>
2021-01-04 01:51:01 +0530

    initial commit

--- lib/Animation/FadeAnimation.dart
+++ lib/Animation/FadeAnimation.dart
@@ -9,7 +9,7 @@ class FadeAnimation extends StatelessWidget {
           .add(Duration(milliseconds: 500), Tween(begin: 0.0, end: 1.0)),
       Track('translateY').add(
           Duration(milliseconds: 500), Tween(begin: -30.0, end: 0.0),
-          curve: Curves.easeOut)
+          curve: Curves.easeOut) 
     ]);
 
     return Container();
